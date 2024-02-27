7082c3f16a51cf5b3c5a8c6d4f581d7e1ece6f43
sri <srikrishnabadi.sgb@gmail.com>
2024-02-27 19:51:33 +0530

    All the necessary skills is updated

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                    My work experience as a associate software engineer and Worked on other leadership activities.
                 </Desc>
                 <TimelineSection>
                     <Timeline>
